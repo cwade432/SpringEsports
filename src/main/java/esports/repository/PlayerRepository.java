9718df6c83bde5451613443a5766a82c58448dbe
cwade432 <cawasle@dmacc.edu>
2021-10-21 18:26:43 -0500

    Initial commit

---
+++ src/main/java/esports/repository/PlayerRepository.java
@@ -0,0 +1,14 @@
+/**
+ * @author Caleb - cawasle
+CIS175 - Fall 2021
+Oct 18, 2021
+ */
+package esports.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import esports.beans.Player;
+
+@Repository
+public interface PlayerRepository extends JpaRepository<Player, Long>{ }
